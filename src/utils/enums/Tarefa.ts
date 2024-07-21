56393b5c9254a71731f29f45330064f1ad035009
Julia Alvim <juliameuportoseguro@gmail.com>
2024-07-21 19:23:09 -0300

    atualizando o filtro

---
+++ src/utils/enums/Tarefa.ts
@@ -0,0 +1,10 @@
+export enum Prioridade {
+  URGENTE = 'uregnte',
+  IMPORTANTE = 'importante',
+  NORMAL = 'normal'
+}
+
+export enum Status {
+  PEDENTE = 'pendente',
+  CONCLUIDA = 'concluída'
+}
