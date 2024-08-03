479330ad1abd4c7da51712105f7599e6c28595e1
Julia Alvim <juliameuportoseguro@gmail.com>
2024-08-03 20:25:13 -0300

    finalizando o projeto

--- src/components/Tarefa/styles.ts
+++ src/components/Tarefa/styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 import variaveis from '../../styles/variaveis'
 
 import * as enums from '../../utils/enums/Tarefa'
+import { Botao } from '../../styles'
 
 type TagProps = {
   prioridade?: enums.Prioridade
@@ -28,12 +29,18 @@ export const Card = styled.div`
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   padding: 16px;
   margin-bottom: 16px;
+
+  label {
+    display: flex;
+    align-items: center;
+    margin-bottom: 16px;
+  }
 `
 
 export const Titulo = styled.h3`
   font-weight: bold;
   font-size: 16px;
-  margin-bottom: 16px;
+  margin-left: 8px;
 `
 
 export const Tag = styled.span<TagProps>`
@@ -64,18 +71,6 @@ export const Descricao = styled.textarea`
 export const BarraAcoes = styled.div`
   border-top: 1px solid regbs(0, 0, 0, 0.1);
   padding-top: 16px;
-`
-
-export const Botao = styled.button`
-  font-size: 12px;
-  font-weight: bold;
-  color: #fff;
-  padding: 8px 12px;
-  border: none;
-  cursor: pointer;
-  background-color: #2f3640;
-  border-radius: 8px;
-  margin-right: 8px;
 `
 
 export const BotaoCancelarRemover = styled(Botao)`
--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -1,52 +1,64 @@
 import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import FiltroCard from '../../components/FiltroCard'
 import * as S from './styles'
 import { alterarTermo } from '../../store/reducers/filtro'
 import { RootReducer } from '../../store'
 import * as enums from '../../utils/enums/Tarefa'
-import { Campo } from '../../styles'
+import { Botao, Campo } from '../../styles'
 
-const BarraLateral = () => {
+type Props = {
+  mostrarFiltros: boolean
+}
+
+const BarraLateral = ({ mostrarFiltros }: Props) => {
+  const navigate = useNavigate()
   const dispatch = useDispatch()
   const { termo } = useSelector((state: RootReducer) => state.filtro)
 
   return (
     <S.Aside>
       <div>
-        <Campo
-          type="text"
-          placeholder="Buscar"
-          value={termo}
-          onChange={(evento) => dispatch(alterarTermo(evento.target.value))}
-        />
-        <S.Filtros>
-          <FiltroCard
-            valor={enums.Status.PEDENTE}
-            criterio="status"
-            legenda="pendentes"
-          />
-          <FiltroCard
-            valor={enums.Status.CONCLUIDA}
-            criterio="status"
-            legenda="concluidas"
-          />
-          <FiltroCard
-            valor={enums.Prioridade.URGENTE}
-            criterio="prioridade"
-            legenda="normal"
-          />
-          <FiltroCard
-            valor={enums.Prioridade.NORMAL}
-            criterio="prioridade"
-            legenda="importante"
-          />
-          <FiltroCard
-            valor={enums.Prioridade.URGENTE}
-            criterio="prioridade"
-            legenda="urgente"
-          />
-          <FiltroCard criterio="todas" legenda="todas" />
-        </S.Filtros>
+        {mostrarFiltros ? (
+          <>
+            <Campo
+              type="text"
+              placeholder="Buscar"
+              value={termo}
+              onChange={(evento) => dispatch(alterarTermo(evento.target.value))}
+            />
+            <S.Filtros>
+              <FiltroCard
+                valor={enums.Status.PEDENTE}
+                criterio="status"
+                legenda="pendentes"
+              />
+              <FiltroCard
+                valor={enums.Status.CONCLUIDA}
+                criterio="status"
+                legenda="concluidas"
+              />
+              <FiltroCard
+                valor={enums.Prioridade.URGENTE}
+                criterio="prioridade"
+                legenda="normal"
+              />
+              <FiltroCard
+                valor={enums.Prioridade.NORMAL}
+                criterio="prioridade"
+                legenda="importante"
+              />
+              <FiltroCard
+                valor={enums.Prioridade.URGENTE}
+                criterio="prioridade"
+                legenda="urgente"
+              />
+              <FiltroCard criterio="todas" legenda="todas" />
+            </S.Filtros>
+          </>
+        ) : (
+          <Botao onClick={() => navigate('/')}>Voltar</Botao>
+        )}
       </div>
     </S.Aside>
   )
